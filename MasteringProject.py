485c7dd2e9c4b4778b96e4950892c9e020ff491a
Paul Ayo <femi4dami@gmail.com>
2018-08-27 19:35:38 +0100

    Learning new concept

--- MasteringProject.py
+++ MasteringProject.py
@@ -1,27 +1,27 @@
-# traveling = input("Are you travelling: ")
-# 
-# while traveling == 'yes' or 'y':
-# 
-#     num = int(input("Fill in number of people travelling: "))
-# 
-#     for num in range(1,num+1):
-# 
-#         name  = input('Name: ')
-#         sex   = input('Male or Female: ')
-#         destination = input('Destination: ')
-# 
-# 
-# 
-# 
-#         print(name)
-# 
-#         print(sex)
-# 
-#         print(destination)
-# 
-# 
-#     traveling = input('Forgot someone? ')
-# 
+traveling = input("Are you travelling: ")
+
+while traveling == 'yes' or 'y':
+
+    num = int(input("Fill in number of people travelling: "))
+
+    for num in range(1,num+1):
+
+        name  = input('Name: ')
+        sex   = input('Male or Female: ')
+        destination = input('Destination: ')
 
 
-# Creating an ATM 
+
+
+        print(name)
+
+        print(sex)
+
+        print(destination)
+
+
+    traveling = input('Forgot someone? ')
+
+
+
+Creating an ATM 
--- stackoverflow.py
+++ stackoverflow.py
@@ -1,58 +1,149 @@
 
 
-import string
-import random
+# import string
+# import random
 
 
-def id_generator(size=6, chars=string.ascii_uppercase+string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+# def id_generator(size=6, chars=string.ascii_uppercase+string.digits):
+#     return ''.join(random.choice(chars) for _ in range(size))
 
-print(id_generator())
+# print(id_generator())
 
-print(id_generator(3, "6793YUIO"))
-
-
-print(string.ascii_uppercase)
-
-print(string.digits)
-
-print(string.ascii_uppercase+string.digits)
-
-print(range(4))
+# print(id_generator(3, "6793YUIO"))
 
 
-print(['elem' for _ in range(4)])
+# print(string.ascii_uppercase)
 
-print(random.choice("abcde"))
+# print(string.digits)
 
-print(random.choice("abcde"))
-print(random.choice("abcde"))
-print(random.choice("abcde"))
-random.choice("abcde")
-print([random.choice('abcde') for _ in range(3)])
-print([random.choice('abcde') for _ in range(3)])
-[random.choice('abcde') for _ in range(3)]
+# print(string.ascii_uppercase+string.digits)
 
-[random.choice('abcde') for _ in range(3)]
-
-print(''.join(['a','b','b']))
-
-print([''.join(random.choice('abcde')) for _ in range(3)])
-
-print([' '.join(random.choice('vaecdeg123')) for _ in range(5)])
-
-print((id_generator()))
+# print(range(4))
 
 
-################
-# how to sum items in list seperately
+# print(['elem' for _ in range(4)])
 
-def count_items(column_list):
-    return [column_list.count(item) for item in set(column_list)]
+# print(random.choice("abcde"))
+
+# print(random.choice("abcde"))
+# print(random.choice("abcde"))
+# print(random.choice("abcde"))
+# random.choice("abcde")
+# print([random.choice('abcde') for _ in range(3)])
+# print([random.choice('abcde') for _ in range(3)])
+# [random.choice('abcde') for _ in range(3)]
+
+# [random.choice('abcde') for _ in range(3)]
+
+# print(''.join(['a','b','b']))
+
+# print([''.join(random.choice('abcde')) for _ in range(3)])
+
+# print([' '.join(random.choice('vaecdeg123')) for _ in range(5)])
+
+# print((id_generator()))
 
 
-    column_list = ['male','male','','','','female','male','female']
+# ################
+# # how to sum items in list seperately
+
+# def count_items(column_list):
+#     return [column_list.count(item) for item in set(column_list)]
 
 
-print(count_items(['male','male','','','','female','male','female']))
+#     column_list = ['male','male','','','','female','male','female']
 
+
+# print(count_items(['male','male','','','','female','male','female']))
+
+
+
+
+#understanding generators in python
+
+def myGen(n):
+    yield n 
+    yield n + 1
+
+
+g = myGen(6)
+next(g)
+
+next(g)
+
+next(g)
+
+for n in myGen(n):
+    print(n)
+
+
+# Generator expressions
+g = (n for n in range(3,5))
+next(g)
+
+next(g)
+# List comprehension
+
+lc = [n for n in range(3, 5)]
+
+print(lc)
+
+
+# Fibonacci Numbers
+
+def fib():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+import itertools
+list(itertools.islice(fib(), 10))
+
+
+
+def myGenerator():
+    yield 'These'
+    yield 'words'
+    yield 'come'
+    yield 'one'
+    yield 'at'
+    yield 'a'
+    yield 'time'
+
+myGeneratorInstance = myGenerator()
+next(myGeneratorInstance)
+
+next(myGeneratorInstance)
+
+for word in myGeneratorInstance:
+    print(word)
+
+
+# Dealing with infinity
+
+from time import gmtime, strftime
+def myGen():
+    while True:
+        yield strftime('%a, %d %b %Y %H:%M:%S +0000', gmtime())
+myGeneratorInstance = myGen()
+next(myGenertorInstance)
+
+next(myGeneratorInstance)
+
+
+# What does the yield keyword do?
+
+def _get_child_candidates(self, distance, min_dist, max_dist):
+    if self._leftchild and distance - max_dist < self._median:
+        yield self._leftchild
+    if self._rightchild and distance + max_dist >= self._median:
+        yield self._rightchild
+        result, candidates = [], [self]
+while candidates:
+    node = candidates.pop()
+    distance = node._get_dist(obj)
+    if distance <= max_dist and distance >= min_dist:
+        result.extend(node._values)
+    candidates.extend(node._get_child_candidates(distance, min_dist, max_dist))
+return result
