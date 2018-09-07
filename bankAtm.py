055240ca64746224c0dd919127d19d2ef51e73c2
Paul Ayo <femi4dami@gmail.com>
2018-09-07 18:58:02 +0100

    Added more small project and tutorials

--- bankAtm.py
+++ bankAtm.py
@@ -3,32 +3,34 @@ import pdb
 
 class Atm():
 
-    def __init__(self,balance):
-        self.balance = float(10,000)
+    def __init__(self):
+        self.balance = float()
         self.transaction_history = []
+        self.password = ''
 
-  
+    def  __str__(self):
+        return f'Your balance is #{self.balance}'
+
     def deposit(self,deposit_amount):
-        self.deposit_amount += self.balance
-        return self.deposit_amount
+        self.balance += deposit_amount
+        return self.balance
 
 
     def customer_pin(self):
-        self.chance = 3
-        self.password[:4] = ''
-        while True:
+        chance = 3
+        password = 1234
+        while self.password != password:
             try:
                 self.password = int(input("Please Enter your four digit pin:  "))
             except ValueError:
                 print('Please Enter the correct password')
-                self.chance -= 1
+                chance -= 1
                 print('You have two attempt left')
                 continue
+
             else:
-                break
-    
-    
+                return f'Your password is correct'
+
 
 customer1 = Atm()
-
-print(customer1.deposit_amount(200))
+print(customer1.deposit(111))
